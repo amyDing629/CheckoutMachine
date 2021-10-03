5c2183ec80cc231facb844b7600db3a3d4a42ec7
Yiyun Ding <yiyunding@Yiyuns-MacBook-Pro.local>
2021-10-03 19:16:52 -0400

    remove src

--- src/mysite/checkout/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('additem', views.additem, name='additem'),
-    path('database_opt', views.database_opt, name='database_opt')
-]
